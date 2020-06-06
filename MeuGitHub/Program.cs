5a9f4effd0861a08a91f04d9206a06e82493c5a2
ThiagoReis83 <reis.fernandes@gmail.com>
2020-06-06 18:56:22 -0300

    Primeira atualização

---
+++ MeuGitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MeuGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
